99be08b717ebe6b8a2acce4d573844a92b061891
homoakin619 <homohaykins619@gmail.com>
2023-06-11 14:33:35 +0100

    added bootstrap js file

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
         <Provider session={session} >
         {children}
         </Provider>
-        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"></Script>
+        <script src="/js/bootstrapjs.js"></script>
       </body>
     </html>
   );
